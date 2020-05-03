2cf23413621a5c5d93e9210be4b43935950cbeb6
mjjecmen <mjjecmen@umich.edu>
2020-05-03 16:08:45 -0400

    figured out the lobby system and player updating/removal with jquery. jquery is sick

--- app.js
+++ app.js
@@ -21,7 +21,11 @@ io.sockets.on('connection', function(socket) {
     console.log('socket connection, ' + socket.name);
     socket.emit('your name', {
         name: socket.name
-    }); // send player their name for display purposes
+    }); // send player their name for display purposes (to everyone in lobby too)
+    socket.broadcast.emit('lobby player added', {
+        name: socket.name,
+        id: socket.id
+    });
     SOCKET_LIST[socket.id] = socket; // push into list
 
     // LOBBY SET UP
@@ -33,20 +37,26 @@ io.sockets.on('connection', function(socket) {
     socket.on('pname update', function(data) {
         console.log(socket.name + ' changed name to ' + data.player_name);
         socket.name = data.player_name;
+        // update those names in peoples lobbies
+        socket.broadcast.emit('lobby name update', {
+            id: socket.id,
+            name: socket.name
+        });
     });
     socket.on('disconnect', function() {
         console.log(socket.name + ' disconnected');
+        socket.broadcast.emit('lobby player removed', {
+            id: socket.id
+        });
         delete SOCKET_LIST[socket.id];
         NUM_SOCKETS--;
     });
 });
 
-// this function gets called continuously
-// have it update players names in the lobby
-setInterval(function(data) {
-    var pack = []; // pack of all player data
-    for(var i in SOCKET_LIST) {
-        socket = SOCKET_LIST[i];
-        
-    }
-}, 1000/25); // 25 frames per second
+// // this function gets called continuously
+// // have it update players names in the lobby
+// setInterval(function(data) {
+//     socket.emit('tick', {
+//         pdata: SOCKET_LIST
+//     });
+// }, 1000/25); // 25 frames per second
