de5f99f49673cb6de5d4e1da8f367914d7a5e264
mjjecmen <mjjecmen@umich.edu>
2020-05-08 21:28:17 -0400

    figured out lobby issues

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ console.log('server started');
 
 // globals for this module
 var IDS_TO_NAMES = {};
+var ROOMS = {}; // keeps track of cards and host for each room
 var CODE_LEN = 4;
 
 var io = require('socket.io')(serv,{});
@@ -38,27 +39,38 @@ io.sockets.on('connection', function(socket) {
     socket.on('hard join', function(code) {
         // join the room, no questions asked
         console.log(socket.id + ' hard join ' + code);
-        socket.join(code);
-        
-        // also notify others in the room of your presence
-        socket.to(code).emit('lobby player added', {
-            name: socket.name,
-            id: socket.id
-        });
-        // and get notified of all the players in the room
-        var players_in_room = io.sockets.adapter.rooms[code].sockets;
-        var lobby_info = {};
-        for(var s in players_in_room) {
-            if(players_in_room.hasOwnProperty(s)) {
-                lobby_info[s] = IDS_TO_NAMES[s];
+
+        // if first in make the lobby
+        if(typeof ROOMS[code] == 'undefined') {
+            // make this player host if first in
+            console.log('first in, making host');
+            ROOMS[code] = {}; // create obj
+            ROOMS[code]['host'] = socket.id;
+            join_then_notify_roommates(socket, code);
+            io.to(socket.id).emit('you are host');
+        } else {
+            // if lobby exists, get notified of all the players in the room
+            var players_in_room = io.sockets.adapter.rooms[code].sockets;
+            var lobby_info = {};
+            for(var s in players_in_room) {
+                if(players_in_room.hasOwnProperty(s)) {
+                    lobby_info[s] = IDS_TO_NAMES[s];
+                }
             }
+            // send this data only to the new player
+            console.log(socket.id + ' is being sent list of players in lobby');
+            io.to(socket.id).emit('players in lobby', lobby_info);
+
+            // then alert others of your presence
+            join_then_notify_roommates(socket, code);
         }
-        // send this data only to the new player
-        console.log(socket.id + ' is being sent list of players in lobby');
-        io.to(socket.id).emit('players in lobby', lobby_info);
     });
     socket.on('host req', function(ack) {
         var lobby = make_lobby_code(CODE_LEN);
+        while(typeof ROOMS[lobby] != 'undefined') {
+            // reroll until we have a unique code
+            lobby = make_lobby_code(CODE_LEN);
+        }
         console.log("creating lobby at code " + lobby);
         // now create the virtual url and io room for that
         app.use('/fishbowl/' + lobby, express.static('client/fishbowl/room'));
@@ -78,7 +90,14 @@ io.sockets.on('connection', function(socket) {
     });
     socket.on('disconnect', function() {
         // broadcast to all players, unfortunate performance hit
-        // but can't figure out how to just broadccast to that player's lobby
+        // but can't figure out how to just broadcast to that player's lobby
+        console.log('player dcd, here are their rooms: ' + socket.rooms);
+        socket.broadcast.emit('DEBUG', socket.rooms);
+        for(var r in socket.rooms) {
+            if(socket.rooms.hasOwnProperty(r)) {
+                // find the lobby room (fmt 'XXXX') and notify the 
+            }
+        }
         delete IDS_TO_NAMES[socket.id];
         socket.broadcast.emit('lobby player removed', {
             id: socket.id
@@ -87,6 +106,27 @@ io.sockets.on('connection', function(socket) {
         // last player DCs
     }); 
 });
+
+// join a room and notify those in the room of your presence (yourself included)
+function join_then_notify_roommates(socket, code) {
+    socket.join(code);
+    // also notify others in the room of your presence (and yourself)
+    io.to(code).emit('lobby player added', {
+        name: socket.name,
+        id: socket.id
+    });
+}
+
+// gets current number of players in the provided lobby
+function num_players(room) {
+    var count = 0;
+    for(var s in room) {
+        if(room.hasOwnProperty(s)) {
+            count++;
+        }
+    }
+    return count;
+}
 
 // makes random char strings for lobby codes
 function make_lobby_code(length) {
